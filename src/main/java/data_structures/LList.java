0a145ea6fe8767584d68ab33d51b861d1c4bc16a
PilotChalkanov <nik.chalkanov@gmail.com>
2024-02-24 18:32:05 +0200

    update jdk make inner class private static

--- src/main/java/data_structures/LList.java
+++ src/main/java/data_structures/LList.java
@@ -26,6 +26,44 @@ public abstract class LList<A> {
             return true;
         }
     }
+    private static class Cons<A> extends LList<A> {
+
+        private final A head;
+        private final LList<A> tail;
+
+        private Cons(A head, LList<A> tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+
+        public A head() {
+            return head;
+        }
+
+        public LList<A> tail() {
+            return tail;
+        }
+
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @SuppressWarnings("unchecked")
+        public static <A> LList<A> list() {
+            return NIL;
+        }
+
+        @SafeVarargs
+        public static <A> LList<A> list(A... a) {
+            LList<A> n = list();
+            for (int i = a.length - 1; i >= 0; i--) {
+                n = new Cons<>(a[i], n);
+            }
+            return n;
+        }
+
+
+    }
 
 }
 
